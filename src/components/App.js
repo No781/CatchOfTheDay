4f281de5dcd27e134cb1a73ffbbf0fedd3e32e3f
No781 <areggeradrian@gmail.com>
2017-03-03 16:00:37 +0100

    added sample FIshes + display

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import sampleFishes from '../sample-fishes'
+import Fish from './Fish'
 
 class App extends React.Component {
     constructor() {
         super();
 
-        this.addFish = this.addFish.bind(this)
+        this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
         // define initial state
         this.state = {
             fishes: {},
@@ -24,14 +27,22 @@ class App extends React.Component {
         // set state
         this.setState({fishes});
     }
+    loadSamples() {
+        this.setState({
+            fishes: sampleFishes
+        });
+    }
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Some Tagline"/>
+                    <ul className="list-of-fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                 <Order/>
-                <Inventory addFish={this.addFish}/>
+                <Inventory loadSamples={this.loadSamples} addFish={this.addFish}/>
             </div>
         )
     }
